981c61471d5aab17837bfd8457b8ef8876c5fd31
EC2 Default User <ec2-user@ip-172-31-5-13.ap-northeast-1.compute.internal>
2023-03-20 06:43:08 +0000

    add serch

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -17,6 +17,6 @@ class RelationshipsController < ApplicationController
 
   def followers
     user = User.find(params[:user_id])
-    @users = user.followers
+    @users = user.followers 
   end
 end
